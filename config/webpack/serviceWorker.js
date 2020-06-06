8a385ec0c48c7303b12878533f7cb65b9afe0b1d
Kore Nordmann <mail@kore-nordmann.de>
2020-06-06 12:52:53 +0200

    Do not cache building data in service worker
    … improves the ligthouse score because we do not block the main thread
    loading all the building data, while it obviously degrades user
    performance.

--- config/webpack/serviceWorker.js
+++ config/webpack/serviceWorker.js
@@ -36,7 +36,7 @@ module.exports = (config, PRODUCTION, SERVER) => {
                     // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
                     navigateFallbackWhitelist: [/^(?!\/__).*/],
                     // Don't precache sourcemaps (they're large) and build asset manifest:
-                    staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+                    staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/, /building\..*\.json$/],
                 }),
             ]
         },
